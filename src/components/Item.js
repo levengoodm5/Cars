b8a60d4b756298d965dd906501de10a5c7fa0190
Mark Levengood <Mark.Levengood@quikrete.com>
2022-05-10 12:53:55 -0400

    Cars project complete.

---
+++ src/components/Item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+//import splitLayoutProps from 'react-native/Libraries/StyleSheet/splitLayoutProps';
+
+const Item = props => {
+    return (
+    <View style={styles.viewStyle}>
+        {props.children}
+    </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: "gainsboro",
+        borderBottomWidth: 0,
+        shadowColor: "black",
+        shadowOffSet: { width: 1, height: 1 },
+        shadowOpacity: 0.1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
+};
+
+export default Item;
